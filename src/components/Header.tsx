ae943d6b61be115b5922f6a1130d506ff6bb2c1a
teegash <140628508+teegash@users.noreply.github.com>
2025-09-02 11:43:03 +0300

    Increase spacing further between Destinations text and chevron arrow to -bottom-3

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -102,7 +102,7 @@ const Header = () => {
               >
                 <span>Destinations</span>
                 {/* Chevron Arrow Below - Overlay */}
-                <div className="absolute -bottom-2 left-1/2 transform -translate-x-1/2">
+                <div className="absolute -bottom-3 left-1/2 transform -translate-x-1/2">
                   <ChevronDown className="w-3 h-3 text-yellow-400" />
                 </div>
               </button>
