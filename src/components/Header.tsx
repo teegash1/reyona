c57c335577bfef9de57e5eb5611ecb8e4cb66f98
teegash <140628508+teegash@users.noreply.github.com>
2025-09-02 11:44:47 +0300

    Remove second chevron arrow, keeping single arrow below Destinations text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -104,7 +104,6 @@ const Header = () => {
                 {/* Chevron Arrow Below - Overlay */}
                 <div className="absolute -bottom-3 left-1/2 transform -translate-x-1/2">
                   <ChevronDown className="w-3 h-3 text-yellow-400" />
-                  <ChevronDown className="w-3 h-3 text-yellow-400" />
                 </div>
               </button>
               
